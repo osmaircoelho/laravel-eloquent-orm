7b48ce75fafcbec1e6c16b4f21eb68391c795de6
Osmair Coelho <osmair.coelho@gmail.com>
2019-03-09 15:25:20 +0000

    Customer model for first relationship

---
+++ app/Models/Client.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model {
+	const TYPE_INDIVIDUAL = 'individual';
+	const TYPE_LEGAL = 'legal';
+
+	const MARITIAL_STATUS = [
+		1 => 'Solteiro',
+		2 => 'Casado',
+		3 => 'Divorciado'
+	];
+
+	protected $fillable = [
+		'name',
+		'document_number',
+		'email',
+		'phone',
+		'defaulter',
+		'date_birth',
+		'sex',
+		'marital_status',
+		'physical_disability',
+		'company_name',
+		'client_type'
+	];
+}
